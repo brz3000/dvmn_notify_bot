946fbe6af3f2fdba7d8d87ceced2d185667fb1f2
brz3000 <45814758+brz3000@users.noreply.github.com>
2023-05-20 01:40:25 +0300

    add .idea in gitignore

--- main.py
+++ main.py
@@ -25,15 +25,15 @@ def main():
         try:
             response = requests.get(url, params=params, headers=headers)
             response.raise_for_status()
-            lp_answer = response.json()
-            if lp_answer["status"] == 'timeout':
-                timestamp = lp_answer["timestamp_to_request"]
-            elif lp_answer["status"] == 'found':
+            long_poplling_answer = response.json()
+            if long_poplling_answer["status"] == 'timeout':
+                timestamp = long_poplling_answer["timestamp_to_request"]
+            elif long_poplling_answer["status"] == 'found':
                 bot = telegram.Bot(token=os.environ['TLG_TOKEN'])
                 chat_id = os.environ['TLG_CHAT_ID']
-                lesson = lp_answer['new_attempts'][0]['lesson_title']
-                lesson_url = lp_answer['new_attempts'][0]['lesson_url']
-                if lp_answer['new_attempts'][0]['is_negative']:
+                lesson = long_poplling_answer['new_attempts'][0]['lesson_title']
+                lesson_url = long_poplling_answer['new_attempts'][0]['lesson_url']
+                if long_poplling_answer['new_attempts'][0]['is_negative']:
                     send_message = bot.send_message(chat_id=chat_id,
                                                     text=f"Hello. Преподаватель проверил работу!\n"
                                                          f"Урок: {lesson}"
@@ -45,9 +45,9 @@ def main():
                                                          f"Урок: {lesson}\n"
                                                          f"Преподавателю всё понравилось, "
                                                          f"можно приступать к следующему уроку")
-                timestamp = response.json()["last_attempt_timestamp"]
+                timestamp = long_poplling_answer["last_attempt_timestamp"]
             else:
-                timestamp = response.json()["timestamp"]
+                timestamp = long_poplling_answer["timestamp"]
         except requests.exceptions.ConnectionError:
             sleep(5)
             pass
