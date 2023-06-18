194eb964315b6fda61e9182c141e24fdf23f25b8
brz3000 <45814758+brz3000@users.noreply.github.com>
2023-06-18 22:02:45 +0300

    Update main.py
    Rename variables

--- main.py
+++ main.py
@@ -39,15 +39,15 @@ def main():
         try:
             response = requests.get(url, params=params, headers=headers)
             response.raise_for_status()
-            answer_from_api_devman = response.json()
-            if answer_from_api_devman["status"] == 'timeout':
-                timestamp = answer_from_api_devman["timestamp_to_request"]
-            elif answer_from_api_devman["status"] == 'found':
+            reviews = response.json()
+            if reviews["status"] == 'timeout':
+                timestamp = reviews["timestamp_to_request"]
+            elif reviews["status"] == 'found':
                 bot = telegram.Bot(token=os.environ['TLG_TOKEN_NOTIFY_BOT'])
                 chat_id = os.environ['TLG_CHAT_ID']
-                lesson = answer_from_api_devman['new_attempts'][0]['lesson_title']
-                lesson_url = answer_from_api_devman['new_attempts'][0]['lesson_url']
-                if answer_from_api_devman['new_attempts'][0]['is_negative']:
+                lesson = reviews['new_attempts'][0]['lesson_title']
+                lesson_url = reviews['new_attempts'][0]['lesson_url']
+                if reviews['new_attempts'][0]['is_negative']:
                     send_message = bot.send_message(chat_id=chat_id,
                                                     text=dedent(f'''Hello.
                                                     Преподаватель проверил работу!
@@ -61,12 +61,11 @@ def main():
                                                     Урок: {lesson}
                                                     Преподавателю всё понравилось,
                                                     можно приступать к следующему уроку'''))
-                timestamp = answer_from_api_devman["last_attempt_timestamp"]
+                timestamp = reviews["last_attempt_timestamp"]
         except requests.exceptions.ConnectionError:
             sleep(5)
         except requests.exceptions.ReadTimeout:
             pass
-
 
 
 if __name__ == '__main__':
